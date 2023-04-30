a12142f1de3d46084da92923e13d2bcb373acaae
Your Namecls <you@example.com>
2023-04-30 13:02:49 +0530

    file2

---
+++ script1.js
@@ -0,0 +1,21 @@
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var speakWord = "Hello";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+var speakWord = "Good Bye";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
