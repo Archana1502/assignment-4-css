f44585c910aacb569c9cdadabe6545377017291e
Your Namecls <you@example.com>
2023-04-30 13:51:04 +0530

    file4

---  script.js
+++  script.js
@@ -1,14 +1,5 @@
 (function () {
-
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-var speakWord = "Hello";
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
-var speakWord = "Good Bye";
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
